2fbd43b07fa10560be2ba77f807a76430bda8e79
codinghyunman2 <tkso1357@naver.com>
2020-08-06 02:18:08 +0900

    배포

--- src/App.js
+++ src/App.js
@@ -1,13 +1,53 @@
-import React from 'react';
-import Potato from "./Potato"; 
+import React from "react";
+import axios from "axios";
+import Movie from "../src/components/Movie";
+import "./App.css";
 
-function App() {
-  return (
-    <div>
-      <h1>hello!</h1>
-      <Potato />
-    </div>
-  ); 
+class App extends React.Component{
+  state = {
+    isLoading: true,
+    movies: []
+  }
+  getMovies = async () => {
+    const {
+      data: {
+        data: { movies }
+       }
+     } = await axios.get("https://yts-proxy.now.sh/list_movies.json?sort_by=rating");
+    this.setState({ movies, isLoading: false });
+  }
+  componentDidMount() {
+   this.getMovies();
+  }
+
+ 
+  render() {
+    const { isLoading, movies } = this.state;
+    return (
+      <section className="container">
+        {isLoading? (
+          <div className="loader">
+            <span className="loader___text">Loading...</span>
+          </div>
+        ) : (
+          <div className="movies">
+            {movies.map(movie => (
+              <Movie 
+                key={movie.id}
+                id={movie.id} 
+                year={movie.year} 
+                title={movie.title} 
+                summary={movie.summary} 
+                poster={movie.medium_cover_image} 
+                genres={movie.genres}
+              />
+            ))}
+          </div>
+        )}
+      </section>
+    )
+  }
 }
+
 
 export default App;
